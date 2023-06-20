47fa6e39117d8a5c3591444a46caf84720b2ebd2
RobbLeitao <robertoleitaov@gmail.com>
2023-06-20 12:21:33 -0300

    Fix clase App. ABM pt1

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,13 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { useState, useEffect } from "react";
 
 const App = () => {
-    const [tareas, setTareas] = useState([])
+    const [tareas, setTareas] = useState([]) // aprender del useState
+    const [descripcion, setDescripcion] = useState("");
 
     const mostrarTareas = async () => {
         const response = await fetch("api/tarea/lista");
 
         if (response.ok) {
-            const data = response.json();
+            const data = await response.json();
             setTareas(data);
         }
         else {
@@ -19,20 +20,60 @@ const App = () => {
     const formatDate = (string) => {
         let options = { year: 'numeric', month: 'long', day: 'numeric' };
         let fecha = new Date(string).toLocaleDateString("es-PE", options);
-        let hora = new Date(string).toLocaleDateString();
+        let hora = new Date(string).toLocaleTimeString();
 
         return fecha + " | " + hora
     }
 
+    // aprender de useEffect
     useEffect(() => {
         mostrarTareas();
     }, [])
+
+    const guardarTarear = async (e) => {
+        e.preventDefault() //Anulo el efecto de recarga que tiene el form por defecto
+
+        const response = await fetch("api/tarea/guardar", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({ descripcion: descripcion })
+        });
+
+        if (response.ok) {
+            setDescripcion("");
+            await mostrarTareas();
+        }
+    }
+
+    const cerrarTarear = async (id) => {
+        const response = await fetch("api/tarea/cerrar/" + id, {
+            method: "DELETE"
+        });
+
+        if (response.ok) {
+            await mostrarTareas();
+        }
+    }
 
     return (
         <div className="container bg-dark p-4 vh-100">
             <h2 className="text-white">Lista de tareas</h2>
             <div className="row">
-                <div className="col-sm-12"></div>
+                <div className="col-sm-12">
+                    <form onSubmit={guardarTarear}>
+                        <div className="input-group">
+                            <input type="text"
+                                className="form-control"
+                                placeholder="Ingresa el coso"
+                                value={descripcion}
+                                onChange={(e) => setDescripcion(e.target.value)}
+                            />
+                            <button className="btn btn-success" type="submit">AGREGAR</button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
             <div className="row mt-4">
@@ -42,9 +83,11 @@ const App = () => {
                             tareas.map(
                                 (item) => (
                                     <div key={item.idTarea} className="list-group-item list-group-item-action">
-                                        <h5>{item.descripcion}</h5>
-                                        <small className="text-muted"> {formatDate(item.fechaRegistro)} </small>
-                                        <button className="btn btn-sm btn-outline-danger">Cerrar</button>
+                                        <h5 className="text-primary">{item.descripcion}</h5>
+                                        <div className="d-flex justify-content-between">
+                                            <small className="text-muted"> {formatDate(item.fechaRegistro)} </small>
+                                            <button onClick={() => cerrarTarear(item.idTarea)} className="btn btn-sm btn-outline-danger">Cerrar</button>
+                                        </div>
                                     </div>
                                 )
                             )
